ba662d1ba0b8aef11228453c85f5cd0e2c83e18e
Chiara Belfiore <Chiara@example.com>
2024-03-14 10:56:48 +0100

    init

---
+++ src/test/java/com/progettone/tasknest/TasknestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progettone.tasknest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasknestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
